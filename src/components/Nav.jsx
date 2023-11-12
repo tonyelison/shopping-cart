5baede1a80c221f93ece5b2355d779506fd33a00
Anthony Elison <tonyelison37@gmail.com>
2023-11-11 23:17:19 -0700

    Add nav semantic tag to navbar, add basic styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import "../styles/nav.css";
 
 const Nav = () => (
-  <>
-    <Link to="/">Home</Link>
-    <Link to="shop">Shop</Link>
-  </>
+  <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="shop">Shop</Link></li>
+    </ul>
+  </nav>
 );
 
 export default Nav;
