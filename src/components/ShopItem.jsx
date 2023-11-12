9704b58e614edc1611472615d52e53184105ae43
Anthony Elison <tonyelison37@gmail.com>
2023-11-11 22:57:12 -0700

    Add prop type validation to ShopItem

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -1,8 +1,14 @@
+import PropTypes from 'prop-types';
+
 const ShopItem = ({ item }) => (
   <li>
     {item.title}<br />
     <img src={item.image} />
   </li>
 );
+
+ShopItem.propTypes = {
+  item: PropTypes.object,
+};
 
 export default ShopItem;
