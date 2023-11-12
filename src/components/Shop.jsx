49a61b7a2c11586ef3a929515cf9508178534aee
Anthony Elison <tonyelison37@gmail.com>
2023-11-11 17:33:20 -0700

    Query fake store api on store page

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,24 @@
+import { useState, useEffect } from 'react';
+
 const Shop = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/category/electronics')
+      .then((response) => response.json())
+      .then((data) => {
+        setIsLoading(false);
+        setData(data);
+    });
+  }, []);
+
   return (
     <>
-      Shop
+      <h1>Shop</h1>
+      <ul>
+        {isLoading ? 'Loading...' : data.map((item) => (<li key={item.title}>{item.title}</li>))}
+      </ul>
     </>
   );
 };
