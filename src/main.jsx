9460ff3144af8aca50384a84df1000c1c2735e81
Anthony Elison <tonyelison37@gmail.com>
2023-11-11 17:15:38 -0700

    Move Nav and Router to components

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Router from './Router.jsx'
+import Router from './components/Router.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
